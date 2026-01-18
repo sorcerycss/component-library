8cb431807d17cbdfecc351fb78facbe1b91b4a06
sorcerycss <sorcerycss@gmail.com>
2026-01-17 18:30:44 -0800

    WIP: add initial cards component

--- component-library-react/src/App.jsx
+++ component-library-react/src/App.jsx
@@ -1,5 +1,6 @@
 import Badge from "./components/Badge"
 import Banner from "./components/Banner"
+import Card from "./components/Card"
 
 function App() {
 
@@ -20,6 +21,7 @@ function App() {
           <Badge key={color} variant={color}>Badge</Badge>
         ))}
       </div>
+      <br></br>
       <div style={{display: 'flex', flexDirection: 'column', gap: '8px', maxWidth: '300px'}}>
         <Banner variant="success" title="Congratulations!" text="Lorem ipsum"/>
         <Banner variant="warning" title="Attention!" text="Lorem ipsum"/>
@@ -30,6 +32,12 @@ function App() {
         <Banner variant="warning" title="Attention!" />
         <Banner variant="error" title="There is a problem with your application" />
         <Banner variant="neutral" title="Update available" />
+      </div>
+      <br></br>
+      <div>
+        <Card>Card here</Card>
+        <br></br>
+        <Card>Card here (hower)</Card>
       </div>
     </>
       

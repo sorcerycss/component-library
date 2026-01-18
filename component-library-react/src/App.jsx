ee74847bdc8c7ce70064d7c01983be84f1f53859
sorcerycss <sorcerycss@gmail.com>
2026-01-17 18:25:12 -0800

    WIP: add banners component (needs polish)

--- component-library-react/src/App.jsx
+++ component-library-react/src/App.jsx
@@ -20,8 +20,16 @@ function App() {
           <Badge key={color} variant={color}>Badge</Badge>
         ))}
       </div>
-      <div>
-        <Banner>Banner here</Banner>
+      <div style={{display: 'flex', flexDirection: 'column', gap: '8px', maxWidth: '300px'}}>
+        <Banner variant="success" title="Congratulations!" text="Lorem ipsum"/>
+        <Banner variant="warning" title="Attention!" text="Lorem ipsum"/>
+        <Banner variant="error" title="There is a problem with your application" text="Lorem ipsum"/>
+        <Banner variant="neutral" title="Update available" text="Lorem ipsum"/>
+        <br></br>
+        <Banner variant="success" title="Congratulations!" />
+        <Banner variant="warning" title="Attention!" />
+        <Banner variant="error" title="There is a problem with your application" />
+        <Banner variant="neutral" title="Update available" />
       </div>
     </>
       
--- component-library-react/src/components/Banner.jsx
+++ component-library-react/src/components/Banner.jsx
@@ -1,5 +1,33 @@
-export default function Banner({ children }) {
+const variants = {
+    success: {
+        backgroundColor: '#ECFDF5',
+        color: '#047857'
+    },
+    warning: {
+        backgroundColor: '#FFFBEB',
+        color: '#B45309'
+    },
+    error: {
+        backgroundColor: '#FEF2F2',
+        color: '#B45309'
+    },
+    neutral: {
+        backgroundColor: '#EFF6FF',
+        color: '#1C51B9'
+    }
+}
+
+export default function Banner({ variant, title, text }) {
+
+    const currentVariant = variants[variant]
+
     return (
-        <>{children}</>
+        <div style={{
+            backgroundColor: currentVariant.backgroundColor,
+            color: currentVariant.color
+        }}>
+           <div>{title}</div>
+           {text && <div>{text}</div>}
+        </div>
     )
 }

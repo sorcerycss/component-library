0340add91e15d0af38dda14a520eab66cb2d5fbb
sorcerycss <sorcerycss@gmail.com>
2026-01-24 16:13:25 -0800

    wip: basic styling for mobile testimonials component (need polish)

--- component-library-react/src/App.jsx
+++ component-library-react/src/App.jsx
@@ -20,53 +20,95 @@ function App() {
 
   return (
     <>
-    {/* Square badges */}
-      <div style={{display: 'flex', gap: '8px'}}>
+    {/* Square badges component */}
+      <div style={{
+        display: 'flex',
+        gap: '8px',
+        marginBottom: '20px'}}>
         {colors.map(color => (
           <Badge key={color} variant={color} shape="square">Badge</Badge>
         ))}
       </div>
-      <br></br>
-      {/* Pill badges */}
-      <div style={{display: 'flex', gap: '8px'}}>
+      
+      {/* Pill badges component */}
+      <div style={{
+        display: 'flex',
+        gap: '8px',
+        marginBottom: '20px'}}>
         {colors.map(color => (
           <Badge key={color} variant={color} shape="pill">Badge</Badge>
         ))}
       </div>
-      <br></br>
+      
       {/* Banners components */}
-      <div style={{display: 'flex', flexDirection: 'column', gap: '8px', maxWidth: '300px'}}>
-        <Banner variant="success" title="Congratulations!" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."/>
-        <Banner variant="warning" title="Attention!" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."/>
-        <Banner variant="error" title="There is a problem with your application" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."/>
-        <Banner variant="neutral" title="Update available" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."/>
-        <br></br>
-        <Banner variant="success" title="Congratulations!" />
-        <Banner variant="warning" title="Attention!" />
-        <Banner variant="error" title="There is a problem with your application" />
-        <Banner variant="neutral" title="Update available" />
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '8px',
+        maxWidth: '300px',
+        marginBottom: '20px'}}>
+        <Banner
+        variant="success"
+        title="Congratulations!"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."/>
+        <Banner
+        variant="warning"
+        title="Attention!"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."/>
+        <Banner
+        variant="error"
+        title="There is a problem with your application"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."/>
+        <Banner
+        variant="neutral"
+        title="Update available"
+        text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."/>
+        <Banner
+        variant="success"
+        title="Congratulations!" />
+        <Banner
+        variant="warning"
+        title="Attention!" />
+        <Banner
+        variant="error"
+        title="There is a problem with your application" />
+        <Banner
+        variant="neutral"
+        title="Update available" />
       </div>
-      <br></br>
+      
       <div style={{
         display: 'flex',
         flexDirection: 'column',
         gap: '20px',
         maxWidth: '384px'
       }}>
-        <Card icon={cloudIcon} title="Easy Deployment" text="Ac tincidunt sapien vehicula erat auctor pellentesque rhocus. Et magna sit morbi lobortis."/>
-        <br></br>
-        <Card icon={cloudIcon} title="Easy Deployment" text="Ac tincidunt sapien vehicula erat auctor pellentesque rhocus. Et magna sit morbi lobortis."/>
+        <Card icon={cloudIcon}
+        title="Easy Deployment"
+        text="Ac tincidunt sapien vehicula erat auctor pellentesque rhocus. Et magna sit morbi lobortis."/>
+        <Card icon={cloudIcon}
+        title="Easy Deployment"
+        text="Ac tincidunt sapien vehicula erat auctor pellentesque rhocus. Et magna sit morbi lobortis."/>
       </div>
-      <br></br>
-      <div style={{}}>
-        <Testimonial image={personPhoto} quote="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. "
-        name="May Andersons" title="Workcation, CTO"/>
-        <br></br>
-        <Testimonial logo={logo} quote="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. "
-        name="May Andersons" title="Workcation, CTO"/>
+      
+      <div>
+        
+        <Testimonial
+        image={personPhoto}
+        quote="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. "
+        name="May Andersons"
+        title="Workcation, CTO"
+        />
+        
+        <Testimonial
+        logo={logo}
+         quote="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. "
+        name="May Andersons"
+        title="Workcation, CTO"
+        />
+
       </div>
     </>
-      
   )
 }
 
--- component-library-react/src/components/Testimonial.jsx
+++ component-library-react/src/components/Testimonial.jsx
@@ -1,11 +1,66 @@
+import quoteIcon from '../assets/quote-icon.png'
+import dotsPattern from '../assets/dots-pattern.png'
+
 export default function Testimonial({ image, logo, name, title, quote }) {
     return (
-        <div>
-            {image && <img src={image} alt="Person photo" />}
+        <div style={{
+            position: 'relative',
+            paddingTop: image ? '90px' : '0',
+            marginTop: '40px',
+            maxWidth: '376px',
+            minHeight: image ? '654px' : '552px'
+        }}>
+           
+            {/* Overlapping person image */}
+            {image && (
+                <img
+                src={image}
+                alt="Person photo"
+                style={{
+                    position: 'absolute',
+                    top: '0',
+                    left: '50%',
+                    transform: 'translateX(-50%)',
+                    width: '344px',
+                    height: '206px',
+                    borderRadius: '12px',
+                    objectFit: 'cover'
+                }}
+              />
+            )}
+
+            {/* Blue card */}
+            <div style={{
+                backgroundColor: image ? '#2545B8' : '#F9FAFB',
+                color: image ? 'white' : '#1F2937',
+                padding: '140px 20px 20px',
+            }}>
+
+            {/* Show image/logo logic */}
             {logo && <img src={logo} alt="Company logo" />}
-            <div>{quote}</div>
-            <div>{name}</div>
-            <div>{title}</div>
+
+            {/* Show quote icon if there's an image */}
+            {image && <img src={quoteIcon} alt="Quote icon" />}
+
+            <div style={{
+                fontSize: '24px',
+                fontWeight: '500',
+                margin: '24px 16px'
+            }}>{quote}</div>
+            <div style={{
+                fontWeight: '700',
+                margin: '24px 16px 0'
+            }}>{name}</div>
+            <div style={{
+                fontWeight: '500',
+                margin: '0 16px 0'
+            }}>{title}</div>
+
+            {/* Show dots pattern if there's a logo */}
+            {logo && <img src={dotsPattern} alt="Dots pattern" /> }
+
+            </div>
+
         </div>
         
     )
